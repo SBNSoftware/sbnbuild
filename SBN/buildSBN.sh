85f93581b3e7396b098b59f485afbbfea202e36d
Wesley <wketchum@fnal.gov>
2021-03-25 17:11:03 -0500

    update/move around build script

--- SBN/buildSBN.sh
+++ SBN/buildSBN.sh
@@ -5,7 +5,11 @@
 # designed to work on Jenkins
 # this is a proof of concept script
 
-echo "sbncode version: $SBN"
+echo "sbncode version: $SBN_VERSION"
+echo "sbncode tag: $SBN"
+echo "sbnobj tag: $SBNOBJ"
+echo "sbnanaobj tag: $SBNANAOBJ"
+echo "sbndaq_artdaq_core tag: $SBNDAQ_ARTDAQ_CORE"
 echo "base qualifiers: $QUAL"
 echo "s qualifier: $SQUAL"
 echo "build type: $BUILDTYPE"
@@ -13,13 +17,8 @@ echo "workspace: $WORKSPACE"
 
 # Get number of cores to use.
 
-if [ `uname` = Darwin ]; then
-  #ncores=`sysctl -n hw.ncpu`
-  #ncores=$(( $ncores / 4 ))
-  ncores=1
-else
-  ncores=`cat /proc/cpuinfo 2>/dev/null | grep -c -e '^processor'`
-fi
+ncores=`cat /proc/cpuinfo 2>/dev/null | grep -c -e '^processor'`
+
 if [ $ncores -lt 1 ]; then
   ncores=1
 fi
@@ -41,10 +40,15 @@ else
   exit 1
 fi
 
+echo "ls /cvmfs/sbn.opensciencegrid.org"
+ls /cvmfs/sbn.opensciencegrid.org
+echo
+
 if [ -f /cvmfs/sbn.opensciencegrid.org/products/sbn/setup ]; then
   if [ -x /cvmfs/grid.cern.ch/util/cvmfs-uptodate ]; then
     /cvmfs/grid.cern.ch/util/cvmfs-uptodate /cvmfs/sbn.opensciencegrid.org/products/sbn
   fi
+  echo "Setting up sbn cvmfs"
   source /cvmfs/sbn.opensciencegrid.org/products/sbn/setup || exit 1
 else
   echo "No sbn setup file found."
@@ -54,17 +58,7 @@ fi
 #setup mrb
 setup mrb || exit 1
 
-# skip around a version of mrb that does not work on macOS
-
-if [ `uname` = Darwin ]; then
-  if [[ x`which mrb | grep v1_17_02` != x ]]; then
-    unsetup mrb || exit 1
-    setup mrb v1_16_02 || exit 1
-  fi
-fi
-
 # Use system git on macos.
-
 if ! uname | grep -q Darwin; then
   setup git || exit 1
 fi
@@ -79,29 +73,36 @@ mkdir -p $WORKSPACE/temp || exit 1
 mkdir -p $WORKSPACE/copyBack || exit 1
 rm -f $WORKSPACE/copyBack/* || exit 1
 cd $WORKSPACE/temp || exit 1
-mrb newDev  -v $SBN -q $QUAL:$BUILDTYPE || exit 1
+mrb newDev  -v $SBN_VERSION -q $QUAL:$BUILDTYPE || exit 1
 
 set +x
 source localProducts*/setup || exit 1
 
-if [ `uname` = Darwin ]; then
-  setup getopt v1_1_6  || exit 1
-fi
-
 set -x
 cd $MRB_SOURCE  || exit 1
 # make sure we get a read-only copy
-mrb g -r -t $SBN sbncode || exit 1
+mrb g -r sbncode@$SBN || exit 1
 
-# Extract sbnobj version from sbncode product_deps
-sbnobj_version=`grep sbnobj $MRB_SOURCE/sbncode/ups/product_deps | grep -v qualifier | awk '{print $2}'`
-echo "sbnobj version: $sbnobj_version"
-mrb g -r -t $sbnobj_version sbnobj || exit 1
+if [ -z "$SBNOBJ" ]; then
+    # Extract sbnobj version from sbncode product_deps
+    SBNOBJ=`grep sbnobj $MRB_SOURCE/sbncode/ups/product_deps | grep -v qualifier | awk '{print $2}'`
+fi
+echo "sbnobj version: $SBNOBJ"
+mrb g -r sbnobj@$SBNOBJ || exit 1
 
-# Extract sbndaq_artdaq_core version from sbncode product_deps
-sbndaq_artdaq_core_version=`grep sbndaq_artdaq_core $MRB_SOURCE/sbncode/ups/product_deps | grep -v qualifier | awk '{print $2}'`
-echo "sbndaq_artdaq_core version: $sbndaq_artdaq_core_version"
-mrb g -r -t $sbndaq_artdaq_core_version sbndaq_artdaq_core || exit 1
+if [ -z "$SBNANAOBJ" ]; then
+    # Extract sbnobj version from sbncode product_deps
+    SBNANAOBJ=`grep sbnanaobj $MRB_SOURCE/sbncode/ups/product_deps | grep -v qualifier | awk '{print $2}'`
+fi
+echo "sbnanaobj version: $SBNANAOBJ"
+mrb g -r sbnanaobj@$SBNANAOBJ || exit 1
+
+if [ -z "$SBNDAQ_ARTDAQ_CORE" ]; then
+    # Extract sbndaq_artdaq_core version from sbncode product_deps
+    SBNDAQ_ARTDAQ_CORE=`grep sbndaq_artdaq_core $MRB_SOURCE/sbncode/ups/product_deps | grep -v qualifier | awk '{print $2}'`
+fi
+echo "sbndaq_artdaq_core version: $SBNDAQ_ARTDAQ_CORE"
+mrb g -r sbndaq_artdaq_core@$SBNDAQ_ARTDAQ_CORE || exit 1
 
 cd $MRB_BUILDDIR || exit 1
 mrbsetenv || exit 1
@@ -118,13 +119,7 @@ larsoft_version=`grep larsoft $MRB_SOURCE/sbncode/ups/product_deps | grep -v qua
 larsoft_dot_version=`echo ${larsoft_version} |  sed -e 's/_/./g' | sed -e 's/^v//'`
 
 # Extract flavor.
-
-flvr=''
-if uname | grep -q Darwin; then
-  flvr=`ups flavor -2`
-else
-  flvr=`ups flavor -4`
-fi
+flvr=`ups flavor -4`
 
 # Construct name of larsoft manifest.
 
@@ -139,14 +134,8 @@ echo
 curl --fail --silent --location --insecure http://scisoft.fnal.gov/scisoft/bundles/larsoft/${larsoft_version}/manifest/${larsoft_manifest} >> $manifest || exit 1
 
 if echo $QUAL | grep -q noifdh; then
-  if uname | grep -q Darwin; then
-   # If this is a macos build, then rename the manifest to remove noifdh qualifier in the name
-    noifdh_manifest=`echo $manifest | sed 's/-noifdh//'`
-    mv $manifest $noifdh_manifest
-  else
-   # Otherwise (for slf builds), delete the manifest entirely.
-    rm -f $manifest
-  fi
+   # Delete the manifest entirely.
+  rm -f $manifest
 fi
 
 # Save artifacts.
