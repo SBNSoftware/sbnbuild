1958fedcb9085a16de476f06066053601609f506
Wesley <wketchum@fnal.gov>
2021-03-25 18:13:27 -0500

    dir name typo

--- SBN/copyToSciSoft_sbn.sh
+++ SBN/copyToSciSoft_sbn.sh
@@ -5,11 +5,11 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX --tmpdir=$HOME/tmp)
 echo $tmp_dir
 
 cd $tmp_dir
-$cur_dir/SciSoftScripts/copyFromJenkins -q c7 sbn-release-build
-$cur_dir/SciSoftScripts/copyFromJenkins -q e19 sbn-release-build
-$cur_dir/SciSoftScripts/copyFromJenkins -q e20 sbn-release-build
+$cur_dir/ScisoftScripts/copyFromJenkins -q c7 sbn-release-build
+$cur_dir/ScisoftScripts/copyFromJenkins -q e19 sbn-release-build
+$cur_dir/ScisoftScripts/copyFromJenkins -q e20 sbn-release-build
 
-$cur_dir/SciSoftScripts/copyToSciSoft *
+$cur_dir/ScisoftScripts/copyToSciSoft *
 
 cd $cur_dir
 rm -rf $tmp_dir
--- SBN/copyToSciSoft_sbnana.sh
+++ SBN/copyToSciSoft_sbnana.sh
@@ -5,13 +5,13 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX --tmpdir=$HOME/tmp)
 echo $tmp_dir
 
 cd $tmp_dir
-$cur_dir/SciSoftScripts/copyFromJenkins -q c7 sbnana-release-build
-$cur_dir/SciSoftScripts/copyFromJenkins -q e19 sbnana-release-build
+$cur_dir/ScisoftScripts/copyFromJenkins -q c7 sbnana-release-build
+$cur_dir/ScisoftScripts/copyFromJenkins -q e19 sbnana-release-build
 #$cur_dir/SciSoftScripts/copyFromJenkins -q e20 sbnana-release-build
 
 rm *.txt
 
-$cur_dur/SciSoftScripts/copyToSciSoft *
+$cur_dur/ScisoftScripts/copyToSciSoft *
 
 cd $cur_dir
 rm -rf $tmp_dir
