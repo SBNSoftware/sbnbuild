edbea6b14a82af38c840bde88de23eb42cd9c237
Wesley <wketchum@fnal.gov>
2021-03-25 18:43:09 -0500

    typo fix, add e20 for sbnana

--- SBN/copyToSciSoft_sbnana.sh
+++ SBN/copyToSciSoft_sbnana.sh
@@ -7,11 +7,11 @@ echo $tmp_dir
 cd $tmp_dir
 $cur_dir/ScisoftScripts/copyFromJenkins -q c7 sbnana-release-build
 $cur_dir/ScisoftScripts/copyFromJenkins -q e19 sbnana-release-build
-#$cur_dir/SciSoftScripts/copyFromJenkins -q e20 sbnana-release-build
+$cur_dir/ScisoftScripts/copyFromJenkins -q e20 sbnana-release-build
 
 rm *.txt
 
-$cur_dur/ScisoftScripts/copyToSciSoft *
+$cur_dir/ScisoftScripts/copyToSciSoft *
 
 cd $cur_dir
 rm -rf $tmp_dir
