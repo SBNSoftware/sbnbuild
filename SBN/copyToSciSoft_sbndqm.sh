74a9978407e7a3de5f9d98349a4f0133f33b069d
Wesley <wketchum@fnal.gov>
2021-08-24 15:50:44 -0500

    add scripts for sbndqm scisoft/cvmfs

---
+++ SBN/copyToSciSoft_sbndqm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cur_dir=$(pwd)
+
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX --tmpdir=$HOME/tmp)
+echo $tmp_dir
+
+cd $tmp_dir
+#$cur_dir/ScisoftScripts/copyFromJenkins -q c7 sbnana-release-build
+#$cur_dir/ScisoftScripts/copyFromJenkins -q e19 sbnana-release-build
+$cur_dir/ScisoftScripts/copyFromJenkins -q e20 sbndqm-release-build
+
+#rm *.txt
+
+$cur_dir/ScisoftScripts/copyToSciSoft *
+
+cd $cur_dir
+rm -rf $tmp_dir
