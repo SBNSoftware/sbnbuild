4b16eb15bbf6977886cde86a72f2c32cfdf1fbaf
Wesley <wketchum@fnal.gov>
2021-08-30 13:13:13 -0500

    add a sbndqm setup script

---
+++ SBN/setup_build_sbndqm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source /cvmfs/fermilab.opensciencegrid.org/products/artdaq/setup
+source /cvmfs/larsoft.opensciencegrid.org/products/setup
+source /cvmfs/sbn.opensciencegrid.org/products/sbn/setup
+
+export MRB_PROJECT=sbndqm
+setup mrb
+setup gitflow
+
+version=$1
+quals=$2
+
+datestring=`date +%d.%m.%Y_%H%M%S`
+
+mydir="BuildAreas/sbndqm_${version}_${datestring}"
+mkdir -p $mydir
+cd $mydir
+mrb newDev -v $version -q $quals
+source localProducts*/setup
