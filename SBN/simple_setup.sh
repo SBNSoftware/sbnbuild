6b1879df582084adb2c23753a4410f6393cec984
Miquel Nebot <miquelnebot@users.noreply.github.com>
2022-07-01 13:36:24 +0100

    update setup after larsoft v09_54_00

--- SBN/simple_setup.sh
+++ SBN/simple_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /cvmfs/larsoft.opensciencegrid.org/products/setup
+source /cvmfs/larsoft.opensciencegrid.org/setup_larsoft.sh
 source /cvmfs/sbn.opensciencegrid.org/products/sbn/setup
 
 setup mrb
